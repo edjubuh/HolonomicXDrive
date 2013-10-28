f995b408e91c982a59e910f75d7b64a593f95e90
edjubuh <edjubuh@gmail.com>
2013-10-27 21:16:12 -0400

    Release Candidate 0.9.1
    Added author attributes
    Removed TestingJoystickRadians.c (moved its contents to
    HolonomicRadians.h)

--- src/HolonomicRadians.h
+++ src/HolonomicRadians.h
@@ -1,3 +1,10 @@
+/*
+* HolonomicRadians.h
+*
+* Licensend under the Lesser General Public License v3.0. http://goo.gl/fB9rz (c) 2013
+*      Author: Elliot Berman
+*/
+
 #define maxMotorSpeed 127
 #define numberOfMotors  4
 
@@ -63,5 +70,29 @@ void HolonomicRadianOutput(float radians, float speed = 1, byte rotation = 0)
 		motor[frontRight] = 0;
 		motor[rearLeft] = 0;
 		motor[rearRight] = 0;
+	}
+}
+
+#define JOY_Y_DEADBAND 20
+#define JOY_X_DEADBAND 20
+
+typedef struct
+{
+	float radians;
+	float speed;
+} PolarJoystick;
+
+void getPolarJoy(float *radians, float *speed, TVexJoysticks joy_x = Ch2, TVexJoysticks joy_y = Ch1) {
+	byte x_val = vexRT[joy_x];
+	byte y_val = vexRT[joy_y];
+	if((abs(x_val) < 20) && (abs(y_val) < 20)) {
+		*radians = 0;
+		*speed = 0;
+	}
+	else {
+		*radians = (PI/2)-atan2(y_val,x_val);
+		float *speed = sqrt((abs(y_val) * abs(y_val)) + (abs(x_val) * abs(x_val)));
+		*speed = *speed/127;
+		if(speed > 1) *speed = 1;
 	}
 }
